dff4373baf1967c72bbe4e9767b91a1dbfa582a4
Manan-Kansal <142139855+Manan-Kansal@users.noreply.github.com>
2023-08-16 18:48:43 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,20 @@
+function def1(){
+    var def_of_functions = "A JavaScript function is a block of code designed to perform a particular task. A JavaScript function is executed when something invokes it (calls it).";
+    document.getElementById('definition1').innerHTML = def_of_functions;
+}
+function way(){
+    var way = "First write Function then write the function name ,we can also write parameters(optional), after that write the code to be executed , then write return  = result statement (optional) . Dont forget to write it all in curly brackets.";
+    document.getElementById('wayOfWriting').innerHTML = way;
+}
+function def2(){
+    var def_of_function = "FUNCTION is used to declare a function";
+    document.getElementById('definition2').innerHTML = def_of_function;
+}
+function def3(){
+    var def_of_function_name = "Name which we will give to the function , it should be a valid identifier and follow the nameing convention.";
+    document.getElementById('definition3').innerHTML = def_of_function_name;
+}
+function def4(){
+    var def_of_parameters = "They are the optional inputs that you can pass to functions . You can define multiple parameters sperated by comma(,).";
+    document.getElementById('definition4').innerHTML = def_of_parameters;
+}
